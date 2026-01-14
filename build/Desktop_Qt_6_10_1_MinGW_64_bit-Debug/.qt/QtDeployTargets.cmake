c3098ef4274351eea63480e60845b692f1a2d484
lawrenceegr <lawrencelangatmi@gmail.com>
2026-01-14 13:23:36 +0300

    clean repo

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_NotepadApp_FILE C:/Users/marcus/Documents/qt-projects/NotepadApp/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/NotepadApp.exe)
+set(__QT_DEPLOY_TARGET_NotepadApp_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_NotepadApp_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
